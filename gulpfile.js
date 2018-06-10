3b084323300a08e1e7e5809e7536e317a1128653
Christopher Carson <chris@nowzoo.com>
2018-06-10 12:16:18 -0400

    center the video on lg

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
 
 gulp.task('sass', function () {
   return gulp.src('./docs/jumbotron/jumbotron.scss')
@@ -9,4 +11,12 @@ gulp.task('sass', function () {
 
 gulp.task('sass:watch', function () {
   gulp.watch('./docs/jumbotron/**/*.scss', ['sass']);
+});
+
+gulp.task('serve', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./docs"
+        }
+    });
 });
